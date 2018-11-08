eec90fc12a98ae6e20f64f91299a61d23c8e7bfa
Brittany Hill <brit.hill10@gmail.com>
2018-11-07 21:16:23 -0600

    Update files

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const isDev = process.env.NODE_ENV === 'development'
 module.exports = {
   mode: isDev ? 'development' : 'production',
   entry: [
-    '@babel/polyfill', // enables async-await
+    '@babel/polyfill',
     './client/index.js'
   ],
   output: {
