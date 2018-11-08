4dd6ab16188d772ad178aa2c8e77460a44749e1a
Ryan Burke <rdburke2891@gmail.com>
2018-11-08 16:52:32 -0600

    got likes to populate

---
+++ seedData/albums.js
@@ -0,0 +1,28 @@
+const albums = [
+  {
+    title: "Creative Commons Volume 2",
+    artistId: "dexter.id",
+    artworkPath: "https://learndotresources.s3.amazonaws.com/workshop/58cff0e769468300041ef9fd/creative_commons_vol_2.jpeg"
+  },
+  {
+    title: "Zenith",
+    artistId: "dexter.id",
+    artworkPath: "https://learndotresources.s3.amazonaws.com/workshop/58cff0e769468300041ef9fd/zenith.jpeg"
+  },
+  {
+    title: "No Nations (Instrumentals)",
+    artistId: "jets.id",
+    artworkPath: "hhttps://learndotresources.s3.amazonaws.com/workshop/58cff0e769468300041ef9fd/no_nations.jpeg"
+  },
+  {
+    title: "Ghosts I-IV",
+    artistId: "nin.id",
+    artworkPath: "https://learndotresources.s3.amazonaws.com/workshop/58cff0e769468300041ef9fd/ghosts_i-iv.jpeg"
+  },
+  {
+    title: "The Slip",
+    artistId: "nin.id",
+    artworkPath: "https://learndotresources.s3.amazonaws.com/workshop/58cff0e769468300041ef9fd/the_slip.jpeg"
+  }
+]
+module.exports = albums
